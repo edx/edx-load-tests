a23ae2d8a1404729a22bbae2608eed3742bb9e0b
Diana Huang <dkh@edx.org>
2017-04-18 11:24:08 -0400

    Add new course home page to the lms tests.

--- loadtests/lms/courseware_views.py
+++ loadtests/lms/courseware_views.py
@@ -30,6 +30,13 @@ class CoursewareViewsTasks(LmsTasks):
         path = 'courseware' + self.course_data.courseware_path
         self.get(path, name='courseware:index')
 
+    @task(10)
+    def course_home(self):
+        """
+        Requests the main course home page that contains the course outline.
+        """
+        self.get('course', name='courseware:course_home')
+
     @task(33)
     def mktg_course_about(self):
         """
