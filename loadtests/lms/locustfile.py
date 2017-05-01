5ff4847450d62c44c9a07163305c1d169f877366
Michael Roytman <mroytman@edx.org>
2017-05-01 15:43:01 -0400

    increase number of forums tasks

--- loadtests/lms/locustfile.py
+++ loadtests/lms/locustfile.py
@@ -134,7 +134,7 @@ class LmsTest(LmsTasks):
 
     tasks = {
         CoursewareViewsTasks: 5,
-        ForumsTasks: 1,
+        ForumsTasks: 15,
         ModuleRenderTasks: int(round(22 * float(settings.data.get('MODULE_RENDER_MODIFIER', 1)))),
         ProctoredExamTasks: int(round(1 * float(settings.data.get('PROCTORED_EXAM_MODIFIER', 1)))),
         TrackingTasks: 24,
