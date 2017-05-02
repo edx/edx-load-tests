d1e241d84054295541ec3cc06bac3d4840cb846a
Michael Roytman <mroytman@edx.org>
2017-05-02 10:58:27 -0400

    lower forums tasks

--- loadtests/lms/locustfile.py
+++ loadtests/lms/locustfile.py
@@ -134,7 +134,7 @@ class LmsTest(LmsTasks):
 
     tasks = {
         CoursewareViewsTasks: 5,
-        ForumsTasks: 15,
+        ForumsTasks: 10,
         ModuleRenderTasks: int(round(22 * float(settings.data.get('MODULE_RENDER_MODIFIER', 1)))),
         ProctoredExamTasks: int(round(1 * float(settings.data.get('PROCTORED_EXAM_MODIFIER', 1)))),
         TrackingTasks: 24,
